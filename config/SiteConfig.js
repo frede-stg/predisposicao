e73c88eec27fadf3acabb3aad2fd63808dd8b2c3
frederico <frede.stg@gmail.com>
2018-09-15 13:02:27 -0300

    edit

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -2,22 +2,22 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
 <<<<<<< HEAD
-  siteTitle: 'Predisposição Poética', // Navigation and Site Title
-  siteTitleAlt: 'Predisposição Poética', // Alternative Site title for SEO
-  siteUrl: 'www.predisposicaopoetica.com', // Domain of your site. No trailing slash!
+  siteTitle: 'Minimal Blog', // Navigation and Site Title
+  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
+  siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Poesias aleatórias', // Your site description
+  siteDescription: 'Minimal Blog with big typography', // Your site description
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  //userTwitter: '@minimal', // Twitter Username - Optional
-  //ogSiteName: 'minimal', // Facebook Site Name - Optional
+  userTwitter: '@minimal', // Twitter Username - Optional
+  ogSiteName: 'minimal', // Facebook Site Name - Optional
 =======
   siteTitle: 'Predisposição Poética', // Navigation and Site Title
   siteTitleAlt: 'Predisposição Poética', // Alternative Site title for SEO
   siteUrl: 'https://predisposicaopoetica.com', // Domain of your site. No trailing slash!
-  siteLanguage: 'pt', // Language Tag on <html> element
+  siteLanguage: 'en', // Language Tag on <html> element
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'TODO', // Your site description
