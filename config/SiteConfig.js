8a4bdd09603cf7fe6655792a9973a20dc3c17914
frederico <frede.stg@gmail.com>
2018-09-15 13:05:08 -0300

    edit

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,19 +1,6 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-<<<<<<< HEAD
-  siteTitle: 'Minimal Blog', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
-  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
-  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with big typography', // Your site description
-
-  // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@minimal', // Twitter Username - Optional
-  ogSiteName: 'minimal', // Facebook Site Name - Optional
-=======
   siteTitle: 'Predisposição Poética', // Navigation and Site Title
   siteTitleAlt: 'Predisposição Poética', // Alternative Site title for SEO
   siteUrl: 'https://predisposicaopoetica.com', // Domain of your site. No trailing slash!
@@ -25,7 +12,6 @@ module.exports = {
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '', // Twitter Username - Optional
   ogSiteName: '', // Facebook Site Name - Optional
->>>>>>> first try
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
